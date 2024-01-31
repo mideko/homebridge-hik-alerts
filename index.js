802b632c85b9af098e6890a4cecf4de1b979dee2
mideko <93245145+mideko@users.noreply.github.com>
2024-01-31 13:52:53 +0100

    Update index.js
    fixed some bad coding

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-/*
- * a very simple implementation of the Camera FFmpeg option to handle motion triggers by sensors
- * by using the Hikvision Nvr alert stream
- */
 let Service, Characteristic;
 const packageJson = require('./package.json');
 const node_xml_stream = require('node-xml-stream');
 const http = require('http');
 const jsdom = require("jsdom");
 //const {JSDOM} = jsdom;
-let hikNvrLog, hikNvrCamID, hikNvrAlert, hikNvrEvent, hikNvrState, hikNvrStamp, hikNvrSensor, hikNvrXmlDoc,hikNvrXmlStart,hikNvrLastAlert,hikNvrThisAlert;
+let hikNvrLog, hikNvrCamID, hikNvrAlert, hikNvrEvent, hikNvrState, hikNvrStamp, hikNvrSensor, hikNvrXmlDoc,hikNvrXmlStart;
 let hikNvrSensors = [];
 let hikNvrCameraFFmpegPort;
 
@@ -19,39 +15,55 @@ module.exports = function (homebridge) {
 }
 
 class hikAlerts {
-constructor(log, config, api) {
+    constructor(log, config, api) {
+        
+        this.req = null;
+        this.log = log;
+        hikNvrLog = log;
+        //get config settings
+        this.config = config;
+        this.name = config.name;
+        this.username = config.user_name;
+        this.password = config.password;
+        //addresss of the nvr:
+        this.nvr =config.nvr_host;
+        this.nvrport =config.nvr_port;
+        //port where ffmpeg or compatible plugin is listening:
+        hikNvrCameraFFmpegPort = config.camera_ffmpeg_porthttp;
+        
+        if (this.username && this.password) {
+            this.auth = this.username+":"+this.password;
+        }
+        this.authIn = 'Basic ' + Buffer.from(this.auth).toString('base64');
+        //options for alertStream from the NVR
+        this.streamOptions = {
+            host: this.nvr,
+            path: '/ISAPI/Event/notification/alertStream',
+            port: this.nvrport,
+            method: 'GET',
+            headers: {'Authorization':this.authIn}
+            };
+        
+        //this.sensors=['keep empty','Cam%20Voortuin','Cam%20Tuinpad','Cam%20Hoek','Cam%20Oprit','Cam%20Test'];
+        hikNvrSensors = config['sensors'];
+        this.log("HikAlerts Initalized for ",hikNvrSensors );
+        this.service = new Service.Switch(this.config.name);
+        this.setup_alert_stream(); //switch on hik alerts by default
+        
+}
     
+setup_alert_stream() {
     
-    this.log = log;
-    hikNvrLog = log;
-    //get config settings
-    this.config = config;
-    this.name = config.name;
-    this.username = config.user_name;
-    this.password = config.password;
-    this.nvr =config.nvr_host;
-    this.nvrport =config.nvr_port;
-    this.isOn = true;
-    hikNvrCameraFFmpegPort = config.camera_ffmpeg_porthttp;
-    if (this.username && this.password) {
-      this.auth = this.username+":"+this.password;
-      }
-    this.authIn = 'Basic ' + Buffer.from(this.auth).toString('base64');
-    //option for alertStream from the NVR
-    this.streamOptions = {
-        host: this.nvr,
-        path: '/ISAPI/Event/notification/alertStream',
-        port: this.nvrport,
-        method: 'GET',
-        headers: {'Authorization':this.authIn}
-    };
-    
-    //this.sensors=['keep empty','Cam%20Voortuin','Cam%20Tuinpad','Cam%20Hoek','Cam%20Oprit','Cam%20Test'];
-    hikNvrSensors = config['sensors'];
-    hikNvrLastAlert = '';
-    this.log("HikAlerts Initalized for ",hikNvrSensors );
-    this.service = new Service.Switch(this.config.name);
-
+    //open the alert stream from the NVR
+    this.req = http.request(this.streamOptions, this.nvrcallback);
+    this.log('Hik Alerts is switched on');
+    this.isOn = true; //alerts is switched ON
+    this.req.on('error', (e) => {
+        this.log('there is problem with the NVR stream request:',e);
+        this.isOn = false; //alerts is switched OFF
+        this.req.abort();
+        });
+    this.req.end();
 }
 
 getServices() {
@@ -78,14 +90,7 @@ setOnCharacteristicHandler (value, callback) {
       /* Log to the console the value whenever this function is called */
     //this.log(`calling setOnCharacteristicHandler`, value);
     if (this.isOn) {
-        this.log('Hik Alerts is switched on');
-    //open the alert stream from the NVR
-        this.req = http.request(this.streamOptions, this.nvrcallback);
-        this.req.on('error', (e) => {
-            this.log('there is problem with the NVR stream request\n',e);
-            this.req.abort();
-            });
-        this.req.end();
+        this.setup_alert_stream();
     }
     else {
         this.req.abort();
@@ -107,21 +112,15 @@ getOnCharacteristicHandler (callback) {
       /* Log to the console the value whenever this function is called */
     //this.log(`calling getOnCharacteristicHandler`, this.isOn)
     if (this.isOn) {
-    //open the alert stream from the NVR
-        this.req = http.request(this.streamOptions, this.nvrcallback);
-        this.req.on('error', (e) => {
-            this.log('there is problem with the NVR stream request',e);
-            this.req.abort();
-            });
-        this.req.end();
+        this.log('Hik Alerts is ON');
     }
     else {
-        this.log('Hik Alerts is switched off');
+        this.log('Hik Alerts is OFF');
     }
 
       /*
        * The callback function should be called to return the value
-       * The first argument in the function should be null unless and error occured
+       * The first argument in the function should be null unless an error occured
        * The second argument in the function should be the current value of the characteristic
        * This is just an example so we will return the value from `this.isOn` which is where we stored the value in the set handler
        */
@@ -129,13 +128,14 @@ getOnCharacteristicHandler (callback) {
     }
     
 nvrcallback (response) {
+    //callback from NVR when event has occured
     response.setEncoding('utf8');
     response.on('data',function(chunk) {
         //hikNvrLog('handling nvr response');
         hikNvrXmlStart = chunk.search('<EventNotificationAlert');
         if (hikNvrXmlStart > 0 ) {
             chunk = chunk.slice(hikNvrXmlStart); //strip any non-xml prefix
-            hikNvrLog(chunk);
+            //hikNvrLog(chunk);
             hikNvrCamID = 0;
             //turn chunk into xml doc that can be queried
             const hikNvrXmlDoc = new jsdom.JSDOM(chunk,{contentType:"application/xml"});
@@ -151,22 +151,18 @@ nvrcallback (response) {
                     
                     hikNvrCamID = parseInt(hikNvrXmlDoc.window.document.querySelector('dynChannelID').textContent)-1;
                     hikNvrSensor = hikNvrSensors[hikNvrCamID];
-                    hikNvrThisAlert = hikNvrStamp+hikNvrSensor+hikNvrEvent;
-                    if (hikNvrThisAlert == hikNvrLastAlert ) {
-                    //trigger related motion switch in camera ffmpeg module if not same as before
-                        var fire = http.get('http://localhost:'+hikNvrCameraFFmpegPort+'/motion?'+hikNvrSensor);
+                    //trigger related motion switch in camera ffmpeg module
+                    var fire = http.get('http://localhost:'+hikNvrCameraFFmpegPort+'/motion?'+hikNvrSensor);
 
-                        fire.on('response', (i) => {
-                          //this.log(i);
-                        });
-                        fire.on('error', (e) => {
-                          //this.log('problem with setting trigger via http: ', e);
-                          fire.abort();
-                        });
-                        //fire.end();
-                        hikNvrLastAlert = hikNvrThisAlert;
-                        hikNvrLog(hikNvrStamp,hikNvrSensor,hikNvrEvent,hikNvrState);
-                    }
+                    fire.on('response', (i) => {
+                      //this.log(i);
+                    });
+                    fire.on('error', (e) => {
+                      //this.log('problem with setting trigger via http: ', e);
+                      fire.abort();
+                    });
+                    //fire.end();
+                    hikNvrLog(hikNvrStamp,hikNvrSensor,hikNvrEvent,hikNvrState);
                 }
                
             }
@@ -176,9 +172,8 @@ nvrcallback (response) {
     });
     response.on('end',function() {
         //this.log('nvr stream ended');
+        //hikNvrLog();
         });
 }
     
 } //class
-
-
